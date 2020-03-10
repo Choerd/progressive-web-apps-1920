4e7058124755908ad1477aa29e141e98e93c0a58
Sjors <sjorspieter@live.nl>
2020-03-10 11:59:21 +0100

    css

---
+++ myapp/modules/data.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch')
+
+const fetcher = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+module.exports = fetcher
+
+
+// const logger = {
+//     info: () => { console.log('info') },
+//     error: () => { console.log('error') }
+// }
+
+// module.exports = logger
--- myapp/server.js
+++ myapp/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const fetch = require('node-fetch')
+const fetcher = require('./modules/data.js')
 
 const app = express()
 const port = 3000
 
-app.use(express.static('static'))
+app.use(express.static('src'))
 
 app.set('view engine', 'ejs');
 app.set('views', 'templates');
@@ -12,8 +12,7 @@ app.set('views', 'templates');
 const apikey = `ebc313f38232bfbdaa36ea5a11721c5f`
 
 app.get('/', async (req, res) => {
-    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apikey}`)
-    const data = await response.json()
+    const data = await fetcher(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apikey}`)
     const genres = data.genres
 
     res.render('home', {
@@ -23,25 +22,19 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/genre/:id', async (req, res) => {
-    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&sort_by=popularity.desc&page=1&with_genres=${req.params.id}`)
-    const data = await response.json()
+    const data = await fetcher(`https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&sort_by=popularity.desc&page=1&with_genres=${req.params.id}`)
     const movies = data.results
 
     res.render('movies', {
-        title: 'Naam',
+        title: 'Categorie',
         movies
     })
 })
 
 app.get('/movie/:id', async (req, res) => {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${apikey}&language=en-US`)
-    const data = await response.json()
-    const movie = data
-
-    console.log(movie)
+    const movie = await fetcher(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${apikey}&language=en-US`)
 
     res.render('movie', {
-        title: 'Naam',
         movie
     })
 })
