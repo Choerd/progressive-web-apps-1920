03d6ea7e684f0c38319c333a90e090c288f2ea31
Sjors <sjorspieter@live.nl>
2020-03-17 13:15:48 +0100

    working service worker

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ app.set('views', 'templates');
 // Determain data per route
 const home = require('./routes/home.js')
 const detail = require('./routes/detail.js')
+const offline = require('./routes/offline.js')
 
 // Routes
 app.get('/', async (req, res) => home(req, res))
 app.get('/movie/:id', async (req, res) => detail(req, res))
+app.get('/offline', (req, res) => offline(req, res))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
--- static/service-worker.js
+++ static/service-worker.js
@@ -1,16 +1,69 @@
-self.addEventListener('install', event => {
+const CORE_CACHE_VERSION = 'v1'
+const CORE_ASSETS = [
+    '/offline',
+    '/bundled/index.css'
+]
 
-    console.log('installing')
+self.addEventListener('install', event => {
+    console.log('Installing Service Worker')
+
+    event.waitUntil(
+        caches.open(CORE_CACHE_VERSION)
+            .then((cache) => cache.addAll(CORE_ASSETS))
+            .then(() => self.skipWaiting())
+    )
 })
 
 self.addEventListener('activate', event => {
-
-    console.log('activate')
+    console.log('Activating service worker')
+    event.waitUntil(clients.claim())
 })
 
 self.addEventListener('fetch', event => {
+    if (isCoreGetRequest(event.request)) {
+        console.log('Core get request: ', event.request.url)
+        event.respondWith(
+            caches.open(CORE_CACHE_VERSION)
+                .then(cache => cache.match(event.request.url))
+        )
+    } else if (isHtmlGetRequest(event.request)) {
+        console.log('html get request', event.request.url)
 
-    console.log('fetch')
-    // event.request.url
-    // event.request.headers
+        event.respondWith(
+            caches.open('html-cache')
+                .then(cache => cache.match(event.request.url))
+                .then(response => response ? response : fetchAndCache(event.request, 'html-cache'))
+                .catch(e => {
+                    return caches.open(CORE_CACHE_VERSION)
+                        .then(cache => cache.match('/offline'))
+                })
+        )
+    }
 })
+
+function fetchAndCache(request, cacheName) {
+    return fetch(request)
+        .then(response => {
+            if (!response.ok) {
+                throw new TypeError('Bad response status');
+            }
+
+            const clone = response.clone()
+            caches.open(cacheName).then((cache) => cache.put(request, clone))
+            return response
+        })
+}
+
+// Check functions
+function isHtmlGetRequest(request) {
+    return request.method === 'GET' && (request.headers.get('accept') !== null && request.headers.get('accept').indexOf('text/html') > -1);
+}
+
+function isCoreGetRequest(request) {
+    return request.method === 'GET' && CORE_ASSETS.includes(getPathName(request.url));
+}
+
+function getPathName(requestUrl) {
+    const url = new URL(requestUrl);
+    return url.pathname;
+}
